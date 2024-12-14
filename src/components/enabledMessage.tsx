16b0ffe1e7f55347fe0042eda9f5a1065dc3eff1
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-12-14 00:17:48 -0500

    add ignore

--- src/components/enabledMessage.tsx
+++ src/components/enabledMessage.tsx
@@ -25,6 +25,7 @@ export const ExpandableMessage = ({ message }: { message: Message }) => {
         className="prose dark:prose-invert prose-sm md:prose-base max-w-none break-words"
         remarkPlugins={[remarkGfm]}
         components={{
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           code({ node, inline, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || "");
             return !inline && match ? (
